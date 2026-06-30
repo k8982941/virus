c43914af61f5e53969f3d776b059b6d3b80d42d7
k8982941 <k8982941@gmail.com>
2026-06-30 09:47:05 -0400

    Add files via upload

---
+++ Sieve of Eratosthenes.py
@@ -0,0 +1,15 @@
+numbohio=input("aristotle asks socrates for a prime number from albert einstein and issac newton ")
+numbohio=int(numbohio)
+avsrweo=True
+
+for x in range(2,numbohio):
+    if numbohio%x==0:
+        avsrweo=False
+        break
+    
+if avsrweo==False:
+    print("ts is NOT prime son 🥀🥀🥀")
+else:
+    print("wow, this is a prime number")
+
+

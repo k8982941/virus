edc255d4859dc4db79c12207143c9456967d993a
k8982941 <k8982941@gmail.com>
2026-06-30 12:53:02 -0400

    Add files via upload

---
+++ cabinet.py
@@ -0,0 +1,22 @@
+cawbinet=[["-", "white powder", "money"],
+          ["passports", "reiobxu", "-"],
+          ["nuclear launch codes", "-", "old sock"]]
+
+print(cawbinet[1][1])
+cawbinet[1][1]="robux"
+print(cawbinet[1][1])
+
+print()
+
+print(cawbinet[0])
+print(cawbinet[1])
+print(cawbinet[2])
+
+print()
+
+for z in range(len(cawbinet)):
+    for iliketeto in range(len(cawbinet[z])):
+        print(cawbinet[z][iliketeto], end=" ")
+    print()
+    
+
